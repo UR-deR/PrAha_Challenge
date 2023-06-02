2aa4e42d23649bd3d0ae4c568257fdc47f6b56f4
william <shota.furuno@claves.co.jp>
2023-06-03 00:06:46 +0900

    wip

--- architecture/PrahaChallenge/backend/src/app/get-all-pairs/usecase.spec.ts
+++ architecture/PrahaChallenge/backend/src/app/get-all-pairs/usecase.spec.ts
@@ -1,28 +1,28 @@
-import { prismaMock } from '../../singleton';
-import { AllPairsDto } from './query-service';
-import { GetAllPairsUsecase } from './usecase';
-import { GetAllPairsQueryService } from '../../infrastructure/query-service/get-all-pairs';
+// import { prismaMock } from '../../singleton';
+// import { AllPairsDto } from './query-service';
+// import { GetAllPairsUsecase } from './usecase';
+// import { GetAllPairsQueryService } from '../../infrastructure/query-service/get-all-pairs';
 
-describe('GetAllPairsUsecase', () => {
-  test('AllPairsDtoを返す', () => {
-    const getAllPairsUseCase = new GetAllPairsUsecase(
-      new GetAllPairsQueryService(),
-    );
+// describe('GetAllPairsUsecase', () => {
+//   test('AllPairsDtoを返す', () => {
+//     const getAllPairsUseCase = new GetAllPairsUsecase(
+//       new GetAllPairsQueryService(),
+//     );
 
-    const allPairs = [
-      {
-        id: 1,
-        name: 'a',
-        team_id: 1,
-      },
-      {
-        id: 2,
-        name: 'b',
-        team_id: 2,
-      },
-    ];
+//     const allPairs = [
+//       {
+//         id: 1,
+//         name: 'a',
+//         team_id: 1,
+//       },
+//       {
+//         id: 2,
+//         name: 'b',
+//         team_id: 2,
+//       },
+//     ];
 
-    prismaMock.pairs.findMany.mockResolvedValue(allPairs);
-    expect(getAllPairsUseCase.do()).resolves.toEqual(new AllPairsDto(allPairs));
-  });
-});
+//     prismaMock.pairs.findMany.mockResolvedValue(allPairs);
+//     expect(getAllPairsUseCase.do()).resolves.toEqual(new AllPairsDto(allPairs));
+//   });
+// });
