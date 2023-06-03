508a326894fd8c7a38cffbfe762f5122b3fee915
william <shota.furuno@claves.co.jp>
2023-06-03 21:26:11 +0900

    Assignmentについての不足しているテストを実装

--- architecture/PrahaChallenge/backend/src/domain/assignment/model.spec.ts
+++ architecture/PrahaChallenge/backend/src/domain/assignment/model.spec.ts
@@ -19,4 +19,22 @@ describe('Assignment', () => {
     const { status } = assignment.markAsDone();
     expect(status).toBe(AssignmentStatus.DONE);
   });
+
+  test('createメソッドによって生成されるインスタンスのstatusはUNTOUCHEDである', () => {
+    const { status } = Assignment.create({
+      title: 'title',
+      description: 'description',
+    });
+
+    expect(status).toBe(AssignmentStatus.UNTOUCHED);
+  });
+
+  test('statusがDONEのAssignmentのstatusを更新しようとするとエラーが投げられる', () => {
+    const doneAssignment = Assignment.create({
+      title: 'title',
+      description: 'description',
+    }).markAsDone();
+
+    expect(() => doneAssignment.markAsPendingReview()).toThrowError();
+  });
 });
