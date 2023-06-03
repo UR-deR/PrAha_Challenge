4d1642bb6d64932c74d24370fb6c3a00208ad95c
william <shota.furuno@claves.co.jp>
2023-06-03 21:07:48 +0900

    Pairエンティティの参加者に過不足がある場合のテストを実装

--- architecture/PrahaChallenge/backend/src/domain/pair/model.spec.ts
+++ architecture/PrahaChallenge/backend/src/domain/pair/model.spec.ts
@@ -24,4 +24,24 @@ describe('Pair Entity', () => {
     expect(pair.pairMemberAttendeeIds).toHaveLength(3);
     expect(pair.pairMemberAttendeeIds).toContainEqual(new AttendeeId('piyo'));
   });
+
+  test('インスタンス生成時に参加者が2人未満の場合エラーを投げる', () => {
+    expect(() =>
+      Pair.create({
+        name: new PairName('a'),
+        pairMemberAttendeeIds: ['hoge'].map((id) => new AttendeeId(id)),
+      }),
+    ).toThrowError();
+  });
+
+  test('インスタンス生成時に参加者が4人以上の場合エラーを投げる', () => {
+    expect(() =>
+      Pair.create({
+        name: new PairName('a'),
+        pairMemberAttendeeIds: ['hoge', 'fuga', 'piyo', 'bazz'].map(
+          (id) => new AttendeeId(id),
+        ),
+      }),
+    ).toThrowError();
+  });
 });
