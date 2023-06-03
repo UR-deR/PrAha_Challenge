649283f3b0557adf0d069942d2c9a8adb3cb7edf
william <shota.furuno@claves.co.jp>
2023-06-03 21:14:48 +0900

    Attendee.createメソッドのstatusについてのテストケースを設けた

--- architecture/PrahaChallenge/backend/src/domain/attendee/model.spec.ts
+++ architecture/PrahaChallenge/backend/src/domain/attendee/model.spec.ts
@@ -3,19 +3,26 @@ import { Email } from '../email/model';
 import { Attendee } from './model';
 
 describe('Attendee', () => {
+  test('createメソッドによって生成されるインスタンスのstatusはACTIVEである', () => {
+    const { status } = Attendee.create({
+      name: 'name',
+      email: new Email('test@test.com'),
+    });
+    expect(status).toBe(AttendeeStatus.ACTIVE);
+  });
   test('resignメソッドによってstatusがRESIGNEDになる', () => {
-    const attendee = Attendee.create({
+    const { status } = Attendee.create({
       name: 'name',
       email: new Email('test@test.com'),
     }).resign();
-    expect((attendee as any).status).toBe(AttendeeStatus.RESIGNED);
+    expect(status).toBe(AttendeeStatus.RESIGNED);
   });
 
   test('stayAwayメソッドによってstatusがSTAY_AWAYになる', () => {
-    const attendee = Attendee.create({
+    const { status } = Attendee.create({
       name: 'name',
       email: new Email('test@test.com'),
     }).stayAway();
-    expect((attendee as any).status).toBe(AttendeeStatus.STAY_AWAY);
+    expect(status).toBe(AttendeeStatus.STAY_AWAY);
   });
 });
