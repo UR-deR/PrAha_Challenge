f3490a484c3ca74df2228b8bd014e77b236699c7
william <shota.furuno@claves.co.jp>
2023-06-10 16:32:19 +0900

    TeamNameが4桁以上はNGという制約を値オブジェクトに反映

--- architecture/PrahaChallenge/backend/src/domain/team-name/model.ts
+++ architecture/PrahaChallenge/backend/src/domain/team-name/model.ts
@@ -1,3 +1,12 @@
 export class TeamName {
-  constructor(public readonly value: number) {}
+  public readonly value: number;
+  constructor(value: number) {
+    if (value > 999) {
+      throw new Error(`Team name must be less than 999 but got ${value}`);
+    }
+    if (value < 0) {
+      throw new Error(`Team name must be greater than 0 but got ${value}`);
+    }
+    this.value = value;
+  }
 }
