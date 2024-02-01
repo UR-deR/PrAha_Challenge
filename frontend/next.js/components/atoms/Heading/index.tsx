4429bd5c7de955c245c16b973ddc88610596c0ab
UR-deR <shota.furuno@claves.co.jp>
2024-02-01 23:38:13 +0900

    Add fontWeight prop to Heading component

--- frontend/next.js/components/atoms/Heading/index.tsx
+++ frontend/next.js/components/atoms/Heading/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import styles from './styles.module.css';
 
 type Props = {
   level: 1 | 2 | 3 | 4 | 5 | 6;
   children: React.ReactNode;
+  fontWeight?: 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 'normal' | 'bold';
 };
 
 export const Heading: React.FC<Props> = (props) => {
-  return <>{React.createElement(`h${props.level}`, {}, props.children)}</>;
+  return (
+    <>
+      {React.createElement(
+        `h${props.level}`,
+        {
+          className: `fontWeight${props.fontWeight ?? 'bold'}`,
+        },
+        props.children
+      )}
+    </>
+  );
 };
