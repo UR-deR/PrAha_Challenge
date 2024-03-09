0bdd118f34556667a1b88a2ab9bc8dc6b920a11f
UR-deR <shota.furuno@claves.co.jp>
2024-03-09 23:12:39 +0900

    display how many times SomeComponent rendered

--- frontend/useEffect/sample/src/some-component.js
+++ frontend/useEffect/sample/src/some-component.js
@@ -1,5 +1,14 @@
+import { useEffect, useState } from 'react';
+
 export const SomeComponent = ({ someFlag }) => {
+  const [renderedCount, setRenderedCount] = useState(0);
+  useEffect(() => {
+    setRenderedCount((count) => count + 1);
+  }, [someFlag]);
   return (
-    <p>ここに、このコンポーネントがレンダリングされた回数を表示してみよう!</p>
+    <>
+      <p>ここに、このコンポーネントがレンダリングされた回数を表示してみよう!</p>
+      <p>レンダリングされた回数: {renderedCount}</p>
+    </>
   );
 };
