5731942541630c1e25d835a8131019eefbbcd03f
UR-deR <shota.furuno@claves.co.jp>
2024-03-10 21:44:24 +0900

    fix fetchComponent.js to fetch GitHub repository information and display

--- frontend/useEffect/sample/src/fetch-component.js
+++ frontend/useEffect/sample/src/fetch-component.js
@@ -1,16 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+
+const fetchGitHubRepository = async (username, reponame) => {
+  const response = await fetch(`https://api.github.com/repos/${username}/${reponame}`);
+  const json = await response.json();
+  return {
+    repository: json,
+  };
+};
 
 export const FetchComponent = () => {
   const [data, setData] = useState({
     subscribers: 0,
-    stars: 0
+    stars: 0,
   });
 
-  // ここでuseEffectを使ってstar数を取得してみましょう!
+  useEffect(() => {
+    fetchGitHubRepository('UR-deR', 'PrAha_Challenge').then(({ repository }) => {
+      setData({
+        subscribers: repository.subscribers_count,
+        stars: repository.stargazers_count,
+      });
+    });
+  }, []);
 
   return (
     <div>
       <p>ここにReactのGitHubレポジトリに付いたスターの数を表示してみよう</p>
+      <p>{data.subscribers} subscribers</p>
       <p>{data.stars} stars</p>
     </div>
   );
