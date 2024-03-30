7f90632767b5359544987eb435e18e6e9a01ba23
UR-deR <shota.furuno@claves.co.jp>
2024-03-30 23:50:44 +0900

    add performance measurement of 1000 times rendering

--- frontend/ref/sample/src/Component.js
+++ frontend/ref/sample/src/Component.js
@@ -1,3 +1,16 @@
-export function SomeComponent() {
+import { useEffect } from 'react';
+
+const FIRST_RENDER = 0;
+const LAST_RENDER = 999;
+
+export function SomeComponent({ index, startTimeRef, endTimeRef, onFinishMeasure }) {
+  useEffect(() => {
+    if (index === FIRST_RENDER) {
+      startTimeRef.current = performance.now();
+    } else if (index === LAST_RENDER) {
+      endTimeRef.current = performance.now();
+      onFinishMeasure(endTimeRef.current - startTimeRef.current);
+    }
+  }, []);
   return <p>Hello!</p>;
 }
--- frontend/ref/sample/src/Profiler.js
+++ frontend/ref/sample/src/Profiler.js
@@ -1,15 +1,34 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useRef } from 'react';
 
 // こちらにパフォーマンス測定のための処理を実装してください
 export function Profiler({ Component, onFinishMeasure }) {
   // 現時点では渡されたComponentを1000回レンダリングしているだけです。
   // useRef, useState, useEffectを活用して、
   // 1000回レンダリングが終了するまでに要した時間をonFinishMeasureに返せるようにしましょう
+  const startTimeRef = useRef(null);
+  const endTimeRef = useRef(null);
+  const [renderFlag, setRenderFlag] = useState(false);
   const children = [];
 
   for (let i = 0; i < 1000; i++) {
-    children.push(<Component key={i} />);
+    children.push(
+      <Component
+        key={i}
+        index={i}
+        startTimeRef={startTimeRef}
+        endTimeRef={endTimeRef}
+        onFinishMeasure={onFinishMeasure}
+      />
+    );
   }
 
-  return <>{children}</>;
+  console.log(startTimeRef, endTimeRef);
+
+  return (
+    <>
+      <button onClick={() => setRenderFlag(true)}>start</button>
+      <br />
+      {renderFlag ? children : null}
+    </>
+  );
 }
